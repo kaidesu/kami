dbc7f983dc8365f8a1ba11dca3f54fa1216e35da
kaidesu <shea.lewis89@gmail.com>
2015-10-22 15:15:12 -0700

    UI clean up

--- app/kami.js
+++ app/kami.js
@@ -20,6 +20,8 @@ editor = new Vue({
 	}
 });
 
+enableTabs('editor-textarea');
+
 function handleDocumentChange(title) {
 	document.title = title;
 }
@@ -105,6 +107,29 @@ function handleSaveButton() {
 			onChosenFileToSave(filename.toString(), true);
 		});
 	}
+}
+
+function enableTabs(id) {
+    var el = document.getElementById(id);
+    el.onkeydown = function(e) {
+        if (e.keyCode === 9) { // tab was pressed
+
+            // get caret position/selection
+            var val = this.value,
+                start = this.selectionStart,
+                end = this.selectionEnd;
+
+            // set textarea value to: text before caret + tab + text after caret
+            this.value = val.substring(0, start) + '\t' + val.substring(end);
+
+            // put caret at right position again
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            return false;
+
+        }
+    };
 }
 
 onload = function() {
